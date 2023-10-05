c3ee63d8f31bf9241fc101dd7e2f92acdcb60fe1
Tirawat <tirawat.po@ku.th>
2023-10-05 23:57:24 +0700

    updated related product in productPage

--- pages/product/index.vue
+++ pages/product/index.vue
@@ -20,18 +20,27 @@
         <section class="flex mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 gap-10 justify-center items-center">
             <!-- Image gallery -->
             <div id="selected" class="m-4 w-24">
-                <div v-for="image in product.images" :key="image.src">
-                    <img class="m-5" src={{image.src}} alt="">
+              <div class="lg:col-span-3 lg:row-end-1">
+                <div class="lg:flex lg:items-start">
+                  <div class="mt-2 w-full lg:order-1 lg:w-32 lg:flex-shrink-0">
+                    <div class="flex flex-row items-start lg:flex-col">
+                      <button type="button" class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-gray-900 text-center">
+                        <img class="h-full w-full object-cover" src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg" alt="" />
+                      </button>
+                      <button type="button" class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
+                        <img class="h-full w-full object-cover" src="https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg" alt="" />
+                      </button>
+                      <button type="button" class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
+                        <img class="h-full w-full object-cover" src="https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg" alt="" />
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <img class="m-5" src="https://image.uniqlo.com/UQ/ST3/th/imagesgoods/424873/item/thgoods_10_424873.jpg?width=750" alt="">
-                <img class="m-5" src="https://image.uniqlo.com/UQ/ST3/th/imagesgoods/424873/item/thgoods_10_424873.jpg?width=750" alt="">
-                <img class="m-5" src="https://image.uniqlo.com/UQ/ST3/th/imagesgoods/424873/item/thgoods_10_424873.jpg?width=750" alt="">
-                <img class="m-5" src="https://image.uniqlo.com/UQ/ST3/th/imagesgoods/454315/item/thgoods_00_454315.jpg?width=750" alt="">
+              </div>
             </div>
             <div id="preview" class="p-6">
                 <img class="m-5 border-2" src="https://image.uniqlo.com/UQ/ST3/th/imagesgoods/454315/item/thgoods_00_454315.jpg?width=750" alt="">
             </div>
-
             <!-- <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
                 <div class="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
                     <img :src="product.images[0].src" :alt="product.images[0].alt" class="h-full w-full object-cover object-center" />
@@ -61,7 +70,7 @@
                     <h2 class="sr-only">Product information</h2>
 
                     <p class=" mt-4 text-3xl tracking-tight text-gray-900">{{ product.price }}</p>
-            
+
                     <form class="mt-10">
                         <!-- Colors -->
                         <div>
@@ -108,7 +117,7 @@
                         <button type="submit" class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add Cart</button>
                     </form>
                 </div>
-                    
+        
                 <div class="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
                     <h3 class="sr-only">Description</h3>
 
@@ -145,18 +154,98 @@
                         </Disclosure>
                     </div>
                         <!-- Description and details -->
-                    <div class="mt-10">
-                        <h2 class="text-sm font-medium text-gray-900">Details</h2>
-                
-                        <div class="mt-4 space-y-6">
-                            <p class="text-lg text-gray-600">{{ product.details }}</p>
+                        <div class="mt-10">
+                            <h2 class="text-sm font-medium text-gray-900">Details</h2>
+                    
+                            <div class="mt-4 space-y-6">
+                                <p class="text-lg text-gray-600">{{ product.details }}</p>
+                            </div>
                         </div>
+                    </div> 
+                </div>
+            </section>
+        </div>
+        <section id="Category Related" class="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 gap-10 justify-center items-center">
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-3xl">Related Product</h1>
+          <!-- card1 -->
+          <div class="relative min-h-screen flex flex-col mt-10 w-1/2 h-1/2">
+            <div class="container">
+              <div class="max-w-md w-full bg-[#DBE2EF] shadow-lg rounded-xl p-6">
+                <div class="flex flex-col ">
+                  <div class="">
+                    <div class="relative h-62 w-full mb-3">
+                      <!-- image -->
+                      <img src="https://image.uniqlo.com/UQ/ST3/th/imagesgoods/450535/item/thgoods_66_450535.jpg?width=750" alt="Just a flower" class=" w-full   object-fill  rounded-2xl">
+                      
+                      <div class="flex mt-4">
+                        <!-- color -->
+                        <div class="flex-1 inline-flex items-center">
+                          <div class="w-full flex-none text-sm flex items-center text-gray-600">
+                            <ul class="flex flex-row justify-center items-center space-x-2">
+                              <li class="">
+                                <span class="block border-2 border-gray-900 hover:border-blue-600 rounded-full transition ease-in duration-300">
+                                  <a href="#blue" class="block w-3 h-3 bg-blue-600 rounded-full"></a>
+                                </span>
+                              </li>
+                              <li class="">
+                                <span class="block border-2 border-gray-900 hover:border-yellow-400 rounded-full transition ease-in duration-300">
+                                  <a href="#yellow" class="block w-3 h-3  bg-yellow-400 rounded-full"></a>
+                                </span>
+                              </li>
+                              <li class="">
+                                <span class="block border-2 border-gray-900 hover:border-red-500 rounded-full transition ease-in duration-300">
+                                  <a href="#red" class="block w-3 h-3  bg-red-500 rounded-full"></a>
+                                </span>
+                              </li>
+                              <li class="">
+                                <span class="block border-2 border-gray-900 hover:border-green-500 rounded-full transition ease-in duration-300">
+                                  <a href="#green" class="block w-3 h-3  bg-green-500 rounded-full"></a>
+                                </span>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                        <!-- Size -->
+                        <div class="lg:flex text-lg text-black">
+                          <div class="flex-1 inline-flex items-center mb-3">
+                            <span class="text-secondary whitespace-nowrap mr-3">Size</span>
+                            <div class="cursor-pointer text-black ">
+                              <span class="hover:text-purple-500 p-1 py-0">S</span>
+                              <span class="hover:text-purple-500 p-1 py-0">M</span>
+                              <span class="hover:text-purple-500 p-1 py-0">L</span>
+                              <span class="hover:text-purple-500 p-1 py-0">XL</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div> 
                     </div>
-                </div> 
+                    <div class="flex-auto justify-evenly">
+                      <div class="flex flex-wrap ">
+                        <div class="w-full flex-none text-lg flex items-center text-gray-600">
+                          <span class="mr-2 mb-2 text-black bg-[#FFFFFF] px-1 border-white rounded-full">Unisex</span>
+                        </div>
+                        <div class="flex items-center w-full justify-between min-w-0 ">
+                          <h2 class="text-xl mr-auto cursor-pointer text-[#112D4E] hover:text-black ">เสื้อสเวตเตอร์ Extra Fine Merino คอกลม แขนยาว</h2>
+                          <div class="flex items-center bg-green-400 text-white text-xs px-2 py-1 ml-3 rounded-lg"> INSTOCK </div>
+                        </div>
+                      </div>
+                      <div class="text-2xl text-black font-semibold mt-1">฿ 240.00</div>
+                      
+                      <div class="flex space-x-2 text-sm font-medium justify-start mt-4">
+                        <button class=" transition ease-in duration-300 inline-flex items-center text-sm font-medium mb-2 md:mb-0 bg-[#b54545] px-5 py-2 hover:shadow-lg tracking-wider text-white rounded-full hover:bg-[#FF0000]">
+                          <span>Add Cart</span>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </section>
     </div>
-</div>
+
+
 </template>
   <script setup>
   import { ref } from 'vue'
@@ -223,5 +312,4 @@
   
   const selectedColor = ref(product.colors[0])
   const selectedSize = ref(product.sizes[2])
-  
   </script>
